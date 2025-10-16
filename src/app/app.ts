c4cd43fdb3809d6b81be30f8429aaebb1aefea2f
AdamIlnicki24 <ilnicki.adam24@gmail.com>
2025-10-16 08:51:42 +0200

    Improve app resource

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  standalone: true,
 })
 export class App {
-  protected readonly title = signal('rekrutacja-wesele-z-klasa');
+  protected readonly title = signal('Test');
 }
