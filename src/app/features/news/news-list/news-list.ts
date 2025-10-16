5204c9561f4a7bf6046dce9deaf2461e9df94e77
AdamIlnicki24 <ilnicki.adam24@gmail.com>
2025-10-16 08:52:33 +0200

    Improve news list resource

--- src/app/features/news/news-list/news-list.ts
+++ src/app/features/news/news-list/news-list.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Header } from '../../../shared/components/header/header';
+import { News } from '../../../shared/interfaces/news';
+import { NewsService } from '../news.service';
+import { FAILED_TO_DOWNLOAD_NEWS_ERROR_MESSAGE } from '../../../shared/constants/errorMessages';
 
 @Component({
   selector: 'app-news-list',
-  imports: [],
+  imports: [CommonModule, Header],
   templateUrl: './news-list.html',
-  styleUrl: './news-list.css'
+  styleUrl: './news-list.css',
+  standalone: true,
 })
-export class NewsList {
+export class NewsList implements OnInit {
+  news: News[] = [];
+  isLoading = false;
+  errorMessage: string | null = null;
 
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.loadNews();
+  }
+
+  loadNews(): void {
+    this.isLoading = true;
+    this.newsService.getNews().subscribe({
+      next: (list) => {
+        this.news = [...list].sort((a, b) => {
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
+        });
+        this.isLoading = false;
+      },
+      error: () => {
+        this.errorMessage = FAILED_TO_DOWNLOAD_NEWS_ERROR_MESSAGE;
+        this.isLoading = false;
+      },
+    });
+  }
 }
