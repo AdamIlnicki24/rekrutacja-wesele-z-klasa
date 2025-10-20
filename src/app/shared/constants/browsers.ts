5f906e44d81ca5f2ed12838a8b045354f62fc27a
AdamIlnicki24 <ilnicki.adam24@gmail.com>
2025-10-20 10:43:19 +0200

    Correct constant

--- src/app/shared/constants/browsers.ts
+++ src/app/shared/constants/browsers.ts
@@ -2,5 +2,5 @@ export const FIREFOX_BROWSER = 'Firefox';
 export const CHROME_BROWSER = 'Chrome';
 export const EDGE_BROWSER = 'Edge';
 export const SAFARI_BROWSER = 'Safari';
-export const OPERA_BROWSER = 'OPERA';
+export const OPERA_BROWSER = 'Opera';
 export const UNKNOWN_BROWSER = 'Przeglądarka nieznana';
